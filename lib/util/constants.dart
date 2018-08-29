0422083d2960ac0e660522a11e883afd4b34d5d8
codeboy <admin@fzwise.com>
2018-08-30 00:34:27 +0800

    依赖及API token 修改

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -1,2 +1,2 @@
 // TODO: To run the app you need an api-key. Get one here: https://developers.themoviedb.org/3/getting-started/introduction
-const String API_KEY = <your-api-key>;
+const String API_KEY = "c6c96756c1befba3bc66fa9da303cc4f";
